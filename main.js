0f77ad2f5b8a8bbff3d08b5dbc2850b07b5776f9
MechCoder0 <30991841+MechCoder0@users.noreply.github.com>
2019-02-11 16:29:58 -0500

    Updating main.js

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ var app = new Vue ({
 		product: 'Socks',
 		description: "This is a pair of socks",
 		image: '\socks.jpeg',
-		google: 'https://www.google.com/'
+		google: 'https://www.google.com/',
+		inStock: this.inventory > 0,
+		inventory: 8,
+		onSale: true
+	}
+});
+
+var banner = new Vue({
+	el:"#banner",
+	data: {
+		banner:true
 	}
 })
